bfe1db62d83a30b25d9c2ced99a42b1199c21246
Kevin Konczak <konczakkevin@gmail.com>
2021-09-08 17:12:15 +0900

    added smooth scrolling

--- source/assets/javascripts/smoothscroll.js
+++ source/assets/javascripts/smoothscroll.js
@@ -1,6 +1,3 @@
-import 'bootstrap';
-
-// Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
